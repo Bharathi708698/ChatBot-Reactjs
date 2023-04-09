bb78ff6e173f50dfeef6635a26cafdc0593cc60b
Bharathi M <119942666+Bharathi708698@users.noreply.github.com>
2023-04-09 20:29:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { MainContainer, ChatContainer, MessageList, Message, MessageInput, Typin
 const systemMessage = { //  Explain things like you're talking to a software professional with 5 years of experience.
   "role": "system", "content": "Explain things like you're talking to a software professional."
 }
-console.log(`${process.env.REACT_APP_API_KEY}`)
+
 function App() {
   const [messages, setMessages] = useState([
     {
